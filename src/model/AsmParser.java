8097e49754e2d2917596cf88966b38d2767c879e
Bogdan <bogdankny@outlook.com>
2022-11-18 18:07:59 +0100

    Ah, y este faltaba

--- src/model/AsmParser.java
+++ src/model/AsmParser.java
@@ -2,8 +2,6 @@ package model;
 
 public class AsmParser {
     private String s;
-    private int pos;
-    static String example = "88110> t 2 PC=6032       br       -4       Tot. Inst: 23  �� Ciclo : 354        FL=1 FE=1 FC=0 FV=0 FR=0 R01 = 00000FB8 h R02 = 00000000 h R03 = 000000F4 h R04 = 00000000 h R05 = 00000000 h R06 = 00000000 h R07 = 00005998 h R08 = 00000000 h R09 = 00000000 h R10 = 00000003 h R11 = 000003FC h R12 = 00000000 h R13 = 00000000 h R14 = 00000000 h R15 = 00000000 h R16 = 00000000 h R17 = 00000000 h R18 = 00000000 h R19 = 00000000 h R20 = 04009E16 h R21 = 00000000 h R22 = 00000000 h R23 = 00000000 h R24 = 00000000 h R25 = 00000000 h R26 = 00000000 h R27 = 00000000 h R28 = 00000000 h R29 = 00000000 h R30 = FFFFFFF8 h R31 = 00000000 h";
 
     public AsmParser(String s) {
         // prepare the string
@@ -26,12 +24,6 @@ public class AsmParser {
                 .replaceFirst(" h", "")
                 .replace(" ", ",");
 
-    }
-
-    public static void main(String[] args) {
-        AsmParser ap = new AsmParser(example);
-        System.out.println(ap.get("PC"));
-        System.out.println(ap.s);
     }
 
     public String get(String key) {
